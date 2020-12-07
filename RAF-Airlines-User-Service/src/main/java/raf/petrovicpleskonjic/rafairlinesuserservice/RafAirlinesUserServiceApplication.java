2e149991cdb397892fc6fc8068faeed99b1659b3
Luka Petrovic <petrovic.luka99@gmail.com>
2020-12-07 20:29:54 +0100

    Initial commit

---
+++ RAF-Airlines-User-Service/src/main/java/raf/petrovicpleskonjic/rafairlinesuserservice/RafAirlinesUserServiceApplication.java
@@ -0,0 +1,13 @@
+package raf.petrovicpleskonjic.rafairlinesuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RafAirlinesUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RafAirlinesUserServiceApplication.class, args);
+	}
+
+}
