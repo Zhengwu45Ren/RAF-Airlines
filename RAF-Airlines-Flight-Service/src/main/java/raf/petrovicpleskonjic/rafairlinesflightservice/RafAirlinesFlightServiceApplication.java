afe5df1da1f62a393339590f199fdc06c86260a5
Luka Petrovic <petrovic.luka99@gmail.com>
2020-12-12 21:46:31 +0100

    Flights service project created

---
+++ RAF-Airlines-Flight-Service/src/main/java/raf/petrovicpleskonjic/rafairlinesflightservice/RafAirlinesFlightServiceApplication.java
@@ -0,0 +1,13 @@
+package raf.petrovicpleskonjic.rafairlinesflightservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RafAirlinesFlightServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RafAirlinesFlightServiceApplication.class, args);
+	}
+
+}
