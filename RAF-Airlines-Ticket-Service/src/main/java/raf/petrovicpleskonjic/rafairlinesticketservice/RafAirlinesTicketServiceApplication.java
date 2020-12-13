8d6fe8fe64902f6b559e6d25efbb0367eda90e0e
Luka Petrovic <petrovic.luka99@gmail.com>
2020-12-13 18:53:28 +0100

    Ticket service project created

---
+++ RAF-Airlines-Ticket-Service/src/main/java/raf/petrovicpleskonjic/rafairlinesticketservice/RafAirlinesTicketServiceApplication.java
@@ -0,0 +1,13 @@
+package raf.petrovicpleskonjic.rafairlinesticketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RafAirlinesTicketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RafAirlinesTicketServiceApplication.class, args);
+	}
+
+}
